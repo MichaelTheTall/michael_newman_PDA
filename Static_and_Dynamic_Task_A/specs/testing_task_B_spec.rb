8ab2cb5ece300fa4af3f8f35e4b3bc741699685b
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-02 18:56:42 +0100

    Task b start@

--- Static_and_Dynamic_Task_A/specs/testing_task_B_spec.rb
+++ Static_and_Dynamic_Task_A/specs/testing_task_B_spec.rb
@@ -9,7 +9,28 @@ class CardGameTest < MiniTest::Test
 @card3 = CardGame.new("hearts", 8)
 
   def test_suit
+    assert_equal("clubs", @card2.suit)
+  end
 
+  def test_value
+    assert_equal(8, @card3.value)
+  end
+
+  def test_ace_check_true
+    assert(checkforAce(@card1))
+  end
+
+  def test_ace_check_false
+    assert_equal(false, checkforAce(@card2))
+  end
+
+  def test_highest_card
+    assert_equal(@card3, highest_card(@card2, @card3))
+  end
+
+  def test_card_total
+    cards = [@card1, @card2, @card3]
+    assert_equal(14, cards_total(cards))
   end
 
 end
